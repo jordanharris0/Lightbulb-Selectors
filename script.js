84776dbe5a6a98104ba74f935a90d2d6f5e72d8a
jordanharris0 <jordanharris1833@yahoo.com>
2025-04-30 16:03:19 -0500

    finished

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // Write your code here
+const lightbulb1 = document.querySelector("#lightbulb1");
+const lightbulb2 = document.querySelector("#lightbulb2");
+const lightbulb3 = document.querySelector("#lightbulb3");
+const h3 = document.querySelector(".subtitle");
+
+let count = 0;
+
+lightbulb1.addEventListener("click", function () {
+  count += 1;
+  h3.textContent =
+    count === 1
+      ? `You've clicked the lights ${count} time`
+      : `You've clicked the lights ${count} times`;
+  lightbulb1.classList.toggle("active");
+});
+lightbulb2.addEventListener("click", function () {
+  count += 1;
+  h3.textContent =
+    count === 1
+      ? `You've clicked the lights ${count} time`
+      : `You've clicked the lights ${count} times`;
+  lightbulb2.classList.toggle("active");
+});
+lightbulb3.addEventListener("click", function () {
+  count += 1;
+  h3.textContent =
+    count === 1
+      ? `You've clicked the lights ${count} time`
+      : `You've clicked the lights ${count} times`;
+  lightbulb3.classList.toggle("active");
+});
